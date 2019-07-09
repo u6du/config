f0c2758e098a5b1746ff7880f7aabc0abc404d3c
- <->
2019-07-09 11:00:26 +0800

    .

--- db.go
+++ db.go
@@ -10,7 +10,7 @@ import (
 
 const DriverName = "sqlite3"
 
-func Db(name, create, insert string, args ...interface{}) *sql.DB {
+func Db(name, create string, args ...interface{}) *sql.DB {
 	name = name + "." + DriverName
 	dbpath, isNew := File.PathIsNew(name)
 
@@ -18,22 +18,33 @@ func Db(name, create, insert string, args ...interface{}) *sql.DB {
 	ex.Panic(err)
 
 	if isNew {
-		db.Exec(create)
-		if len(args) > 0 {
-			s, err := db.Prepare(insert)
-			ex.Panic(err)
+		_, err := db.Exec(create)
+		ex.Panic(err)
 
-			for _, i := range args {
-				t := reflect.TypeOf(i)
-				switch t.Kind() {
-				case reflect.Interface:
-					li, _ := i.([]interface{})
-					_, err = s.Exec(li...)
-				default:
-					_, err = s.Exec(i)
+		argsLen := len(args)
+		if argsLen > 0 {
+			insert := args[0].(string)
+
+			if argsLen > 1 {
+				s, err := db.Prepare(insert)
+				ex.Panic(err)
+
+				for _, i := range args[1:] {
+					t := reflect.TypeOf(i)
+					switch t.Kind() {
+					case reflect.Interface:
+						li, _ := i.([]interface{})
+						_, err = s.Exec(li...)
+					default:
+						_, err = s.Exec(i)
+					}
+					ex.Panic(err)
+
 				}
+			} else {
+				_, err := db.Exec(insert)
+				ex.Panic(err)
 			}
-			ex.Panic(err)
 		}
 	}
 
